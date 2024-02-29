abf46c2d898de7893d742c596b42ecc76321ee94
guoqingshan <1072818264@qq.com>
2024-02-29 22:30:06 +0800

    feat: 1、更新compose版本至1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.androidApplication).apply(false)
+    alias(libs.plugins.jetbrainsCompose).apply(false)
     kotlin("android").version("1.9.21").apply(false)
     kotlin("multiplatform").version("1.9.21").apply(false)
-    id("org.jetbrains.compose").version("1.5.11").apply(false)
     kotlin("native.cocoapods").version("1.9.21").apply(false)
-
 }
